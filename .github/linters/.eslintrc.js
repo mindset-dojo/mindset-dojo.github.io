c1822b3e88dfa76d601e82345d3f924197443088
Computer8543 <103224936+Computer8543@users.noreply.github.com>
2025-06-14 09:10:53 -0400

    Add JSONC support to .eslintrc.js while keeping the Markdown and Prettier setup.

--- .github/linters/.eslintrc.js
+++ .github/linters/.eslintrc.js
@@ -1,22 +1,52 @@
+// .eslintrc.js
 module.exports = {
   root: true,
-  env: { browser: true, es2021: true, node: true },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  plugins: [
+    "md",
+    "jsonc"
+  ],
   extends: [
     "eslint:recommended",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",           // runs Prettier as an ESLint rule
+    "plugin:jsonc/recommended-with-jsonc",    // JSONC core rules
+    "plugin:jsonc/prettier"                  // turn off JSON rules conflicting with Prettier
   ],
-  parserOptions: { ecmaVersion: 12, sourceType: "module" },
-  plugins: ["md"],
   overrides: [
+    // Markdown code-block linting
     {
       files: ["*.md"],
       plugins: ["md"],
-      extends: ["plugin:md/recommended"]
+      extends: ["plugin:md/recommended"],
+      rules: {
+        // you can add or tweak markdown-specific rules here
+      }
+    },
+
+    // JSON / JSONC linting
+    {
+      files: ["*.json", "*.jsonc"],
+      parser: "jsonc-eslint-parser",
+      rules: {
+        "jsonc/auto": "error",         // catch structural problems
+        "jsonc/no-comment": "off"      // allow comments in JSONC
+      }
     }
   ],
   rules: {
+    // JavaScript quality rules
     "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "no-console": ["warn", { allow: ["warn", "error"] }],
+
+    // Prettier formatting enforcement
     "prettier/prettier": "error"
   }
 };
