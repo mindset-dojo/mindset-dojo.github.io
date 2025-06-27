af12d85a6278602918f021fefe92210fec4abd07
Computer8543 <103224936+Computer8543@users.noreply.github.com>
2025-06-27 12:38:35 -0400

    Change strings from double quote to single quote in md.js.

--- assets/js/md.js
+++ assets/js/md.js
@@ -1,9 +1,9 @@
 function initMD () {
   // moblie nav toggle
-  let body = document.querySelector("body");
-  let nav_menu_toggle = document.querySelector("header button");
-  nav_menu_toggle.addEventListener("click", function() {
-    body.classList.toggle("md-no-scroll");
+  let body = document.querySelector('body');
+  let nav_menu_toggle = document.querySelector('header button');
+  nav_menu_toggle.addEventListener('click', function() {
+    body.classList.toggle('md-no-scroll');
     this.ariaExpanded = this.ariaExpanded !== 'true';
   });
 }
