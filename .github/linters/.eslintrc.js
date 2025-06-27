ebd6fd5149c6bac6dc12c8b9a88edabe03267da3
Computer8543 <103224936+Computer8543@users.noreply.github.com>
2025-06-27 13:20:06 -0400

    Add in full name of Markdown plugin to attempt to resolve ESLint error in .eslintrc.js.

--- .github/linters/.eslintrc.js
+++ .github/linters/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:jsonc/recommended-with-jsonc",
-    "plugin:markdown/recommended"
+    "plugin:eslint-plugin-markdown/recommended"
   ],
   overrides: [
     {
