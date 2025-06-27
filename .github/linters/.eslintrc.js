e9086010caa8febfdd3bdfb1bffb3da50d797fed
Computer8543 <103224936+Computer8543@users.noreply.github.com>
2025-06-27 13:11:56 -0400

    Add correct name for Markdown ESLint plugin in .eslintrc.js.

--- .github/linters/.eslintrc.js
+++ .github/linters/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:jsonc/recommended-with-jsonc",
-    "plugin:md/recommended"
+    "plugin:markdown/recommended"
   ],
   overrides: [
     {
