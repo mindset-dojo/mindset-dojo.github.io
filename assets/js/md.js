bd458771af2ec529e74c06781d757d1af2eed86e
Computer8543 <103224936+Computer8543@users.noreply.github.com>
2025-06-27 12:35:33 -0400

    Adjust spacing in md.js.

--- assets/js/md.js
+++ assets/js/md.js
@@ -1,10 +1,10 @@
 function initMD () {
-    // moblie nav toggle
-    var body = document.querySelector("body");
-    var nav_menu_toggle = document.querySelector("header button");
-    nav_menu_toggle.addEventListener("click", function() {
-        body.classList.toggle("md-no-scroll");
-        this.ariaExpanded = this.ariaExpanded !== 'true';
-    });
+  // moblie nav toggle
+  var body = document.querySelector("body");
+  var nav_menu_toggle = document.querySelector("header button");
+  nav_menu_toggle.addEventListener("click", function() {
+    body.classList.toggle("md-no-scroll");
+    this.ariaExpanded = this.ariaExpanded !== 'true';
+  });
 }
 document.addEventListener('DOMContentLoaded', initMD);
