8df2e915a839af44035fd6c8a8fc31f174d2e506
Computer8543 <103224936+Computer8543@users.noreply.github.com>
2025-06-27 12:43:39 -0400

    Add space before function parathesis in md.js.

--- assets/js/md.js
+++ assets/js/md.js
@@ -2,7 +2,7 @@ function initMD () {
   // moblie nav toggle
   let body = document.querySelector('body');
   let navMenuToggle = document.querySelector('header button');
-  navMenuToggle.addEventListener('click', function() {
+  navMenuToggle.addEventListener('click', function () {
     body.classList.toggle('md-no-scroll');
     this.ariaExpanded = this.ariaExpanded !== 'true';
   });
