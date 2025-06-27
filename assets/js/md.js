2696d02216100a4a20b735934cb1263c6dfaab1c
Computer8543 <103224936+Computer8543@users.noreply.github.com>
2025-06-27 12:49:42 -0400

    Change let to const in md.js.

--- assets/js/md.js
+++ assets/js/md.js
@@ -1,7 +1,7 @@
 function initMD () {
   // moblie nav toggle
-  let body = document.querySelector('body')
-  let navMenuToggle = document.querySelector('header button')
+  const body = document.querySelector('body')
+  const navMenuToggle = document.querySelector('header button')
   navMenuToggle.addEventListener('click', function () {
     body.classList.toggle('md-no-scroll')
     this.ariaExpanded = this.ariaExpanded !== 'true'
