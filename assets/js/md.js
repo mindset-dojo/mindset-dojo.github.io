ff0467c74332725a9d43e5ee6d5ed9a9a1e240ef
Computer8543 <103224936+Computer8543@users.noreply.github.com>
2025-06-27 12:45:31 -0400

    Remove extra semicolons in md.js.

--- assets/js/md.js
+++ assets/js/md.js
@@ -1,10 +1,10 @@
 function initMD () {
   // moblie nav toggle
-  let body = document.querySelector('body');
-  let navMenuToggle = document.querySelector('header button');
+  let body = document.querySelector('body')
+  let navMenuToggle = document.querySelector('header button')
   navMenuToggle.addEventListener('click', function () {
-    body.classList.toggle('md-no-scroll');
-    this.ariaExpanded = this.ariaExpanded !== 'true';
-  });
+    body.classList.toggle('md-no-scroll')
+    this.ariaExpanded = this.ariaExpanded !== 'true'
+  })
 }
-document.addEventListener('DOMContentLoaded', initMD);
+document.addEventListener('DOMContentLoaded', initMD)
