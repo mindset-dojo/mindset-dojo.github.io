8f5bfb836d1c7ef9ae4db23186d27b4200ad27aa
Computer8543 <103224936+Computer8543@users.noreply.github.com>
2025-07-02 06:38:33 -0400

    Add React plugin to plugin section in .eslintrc.js.

--- .github/linters/.eslintrc.js
+++ .github/linters/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2021: true, node: true },
   parserOptions: { ecmaVersion: 12, sourceType: "module" },
-  plugins: ["jsonc"],
+  plugins: ["react", "jsonc"],
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
